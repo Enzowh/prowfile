034f0c4ef4e47350ff7416441231350a6bbf106c
Enzowh <111686484+Enzowh@users.noreply.github.com>
2022-10-26 10:22:13 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# prowfile
+# My portfolio
+# [Portfolio](https://enzowh.github.io/profile/)
---
+++ scripts.js
@@ -0,0 +1,3 @@
+function renzow(){
+    document.getElementById('demo').innerHTML = 'renz.cortel14344@gmail.com'
+}
